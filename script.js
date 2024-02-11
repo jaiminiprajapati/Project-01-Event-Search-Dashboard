a2c66e8e22f575f9e9a9c589b72288d47e90ca0e
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-02-11 03:15:39 +0000

    comment unused console log

--- script.js
+++ script.js
@@ -11,10 +11,9 @@ $(document).ready(function () {
 
   getDataFromSeatgeekByCityName()
     .then(serializedSeatgeekDataByUpcomingEvents)
-    .then(function () {
-      //TODO fetch data from thistle
-      console.log(events);
-    })
+    // .then(function () {
+    //   console.log(events);
+    // })
     .catch(function (error) {
       console.error(error);
     });
