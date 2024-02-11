eb20d0340d346e8ce10859f4e7a7e9b0210ea595
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-02-11 00:42:28 +0000

    add function getUserLocation

--- js/api/ipAddress.js
+++ js/api/ipAddress.js
@@ -1,27 +1,28 @@
 var ipAddress;
 var locationInfo;
-function getIPFromLocalStorage(){
-    ipAddress = JSON.parse(localStorage.getItem("ipAddress"));
-    console.log(ipAddress);
-    if(ipAddress === null){
-        getUserLocationByIPAddress();
-    }
+
+function getIPFromLocalStorage() {
+  ipAddress = JSON.parse(localStorage.getItem("ipAddress"));
+  // console.log(ipAddress);
+  if (ipAddress === null) {
+    getUserLocationByIPAddress();
+  }
 }
+
 function getUserLocationByIPAddress() {
-    const fetch_url = "https://ipinfo.io/json?token=4188a5f1213d7f";
-    fetch(fetch_url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            locationInfo = data;
-            localStorage.setItem("ipAddress", JSON.stringify(data));
-        })
-        .catch(() => {
-            console.log("Unable to retrieve data");
-            cityName = "London";
-        });
-} 
+  fetch(URL_ADDRESS.ipInfo + TOKEN.ipInfo)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      locationInfo = data;
+      localStorage.setItem("ipAddress", JSON.stringify(data));
+    })
+    .catch(() => {
+      console.log("Unable to retrieve data");
+      cityName = "London";
+    });
+}
 
 // Example for event listener
 // $("#test-button").on("click", getIPFromLocalStorage);
--- script.js
+++ script.js
@@ -1,117 +1,112 @@
 $(document).ready(function () {
-  var cityName = "";
+  // function serializedSeatgeekUpcomingEvents() {
+  //   seatGeekData.forEach((event) => {
+  //     if (event.venue.has_upcoming_events === true) {
+  //       eventsList.push({
+  //         eventId: event.id,
+  //         location: {
+  //           country: event.venue.country,
+  //           city: event.venue.city,
+  //           state: event.venue.state,
+  //           address: event.venue.address,
+  //           postCode: event.venue.postal_code,
+  //           lat: event.venue.location.lat,
+  //           lon: event.venue.location.lon,
+  //         },
+  //         eventInfo: {
+  //           title: event.title,
+  //           category: event.type,
+  //           dateTimeUTC: event.datetime_utc,
+  //           description: event.description,
+  //         },
+  //         imagesUrls: [
+  //           event.performers[0].image,
+  //           event.performers[0].images.huge,
+  //         ],
+  //         websiteUrl: {
+  //           buyThicketsUrl: event.url,
+  //           findThicketsUrl: event.venue.url,
+  //         },
+  //       });
+  //     }
+  //   });
+  // }
 
-  var eventsList = [];
+  // // TODO finish- combine seatgeek data and thistle Data in eventsList
+  // function mergeEventsData() {}
 
-  var seatGeekData = [];
-  var thistleData = [];
+  // function getDatathistleEventsByLocation(cityName) {
+  //   var headers = new Headers();
 
-  function serializedSeatgeekUpcomingEvents() {
-    seatGeekData.forEach((event) => {
-      if (event.venue.has_upcoming_events === true) {
-        eventsList.push({
-          eventId: event.id,
-          location: {
-            country: event.venue.country,
-            city: event.venue.city,
-            state: event.venue.state,
-            address: event.venue.address,
-            postCode: event.venue.postal_code,
-            lat: event.venue.location.lat,
-            lon: event.venue.location.lon,
-          },
-          eventInfo: {
-            title: event.title,
-            category: event.type,
-            dateTimeUTC: event.datetime_utc,
-            description: event.description,
-          },
-          imagesUrls: [
-            event.performers[0].image,
-            event.performers[0].images.huge,
-          ],
-          websiteUrl: {
-            buyThicketsUrl: event.url,
-            findThicketsUrl: event.venue.url,
-          },
-        });
-      }
-    });
-  }
+  //   headers.set("Authorization", "Bearer " + DATATHISTLE_API_KEY);
 
-  // TODO finish- combine seatgeek data and thistle Data in eventsList
-  function mergeEventsData() {}
+  //   fetch("https://api.datathistle.com/v1/events?location=" + cityName, {
+  //     headers: headers,
+  //   })
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (data) {
+  //       thistleData.push(data);
+  //     });
+  // }
 
-  function getDatathistleEventsByLocation(cityName) {
-    var headers = new Headers();
+  // function getSeatgeekEventsByVenue(cityName) {
+  //   const url =
+  //     "https://api.seatgeek.com/2/events?venue.city=" +
+  //     cityName +
+  //     "&client_id=" +
+  //     SEATGEEK_API_KEY;
 
-    headers.set("Authorization", "Bearer " + DATATHISTLE_API_KEY);
+  //   fetch(url)
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (data) {
+  //       data.events.forEach((event) => {
+  //         seatGeekData.push(event);
+  //       });
+  //       // make sure that the serialization is called only when data is received
+  //       serializedSeatgeekUpcomingEvents();
+  //     });
+  // }
 
-    fetch("https://api.datathistle.com/v1/events?location=" + cityName, {
-      headers: headers,
-    })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        thistleData.push(data);
-      });
-  }
+  // function createCard(event) {
+  //   const id = event.eventId;
+  //   const title = event.eventInfo.title;
+  //   const buyThicketsURL = event.websiteUrl.buyThicketsUrl; // TODO add button for buy tickets
+  //   const findThicketsURL = event.websiteUrl.findThicketsUrl;
+  //   const imgUrl = event.imagesUrls[0]; //TODO take first img if is not there take second if is not there generate random img
+  //   const altDescription = "TODO: alt text dynamically"; //? how to do dynamically img description
+  //   const footerText = "Some description, location, etc..."; //? to choose correct props etc
 
-  function getSeatgeekEventsByVenue(cityName) {
-    const url =
-      "https://api.seatgeek.com/2/events?venue.city=" +
-      cityName +
-      "&client_id=" +
-      SEATGEEK_API_KEY;
+  //   var card = `<div id="event-card-${id}" class="col">
+  //                 <div class="card h-100">
+  //                   <a href="${findThicketsURL}">
+  //                     <img src="${imgUrl}" alt="${altDescription} class="card-img-top"/>
+  //                   </a>
+  //                   <div class="card-body">
+  //                     <h5 class="card-title">${title}</h5>
+  //                   </div>
+  //                   <div class="card-footer">
+  //                     <small class="text-body-secondary">${footerText}</small>
+  //                   </div>
+  //                   <a href="${buyThicketsURL}">
+  //                     <button>Tickets</button>
+  //                   </a>
+  //                 </div>
+  //               </div>`;
 
-    fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        data.events.forEach((event) => {
-          seatGeekData.push(event);
-        });
-        // make sure that the serialization is called only when data is received
-        serializedSeatgeekUpcomingEvents();
-      });
-  }
+  //   return card;
+  // }
 
-  function createCard(event) {
-    const id = event.eventId;
-    const title = event.eventInfo.title;
-    const buyThicketsURL = event.websiteUrl.buyThicketsUrl; // TODO add button for buy tickets
-    const findThicketsURL = event.websiteUrl.findThicketsUrl;
-    const imgUrl = event.imagesUrls[0]; //TODO take first img if is not there take second if is not there generate random img
-    const altDescription = "TODO: alt text dynamically"; //? how to do dynamically img description
-    const footerText = "Some description, location, etc..."; //? to choose correct props etc
+  // function renderEventsSection() {
+  //   eventsList.forEach((event) => {
+  //     eventsSectionRowContainer.append(createCard(event));
+  //   });
+  // }
 
-    var card = `<div id="event-card-${id}" class="col">
-                  <div class="card h-100">
-                    <a href="${findThicketsURL}">
-                      <img src="${imgUrl}" alt="${altDescription} class="card-img-top"/>
-                    </a>
-                    <div class="card-body">
-                      <h5 class="card-title">${title}</h5>
-                    </div>
-                    <div class="card-footer">
-                      <small class="text-body-secondary">${footerText}</small>
-                    </div>
-                    <a href="${buyThicketsURL}">
-                      <button>Tickets</button>
-                    </a>
-                  </div>
-                </div>`;
-
-    return card;
-  }
-
-  function renderEventsSection() {
-    eventsList.forEach((event) => {
-      eventsSectionRowContainer.append(createCard(event));
-    });
-  }
+  function getUserLocation() {}
 
   searchBtn.click(function () {
     cityName = $(inputEl).val();
